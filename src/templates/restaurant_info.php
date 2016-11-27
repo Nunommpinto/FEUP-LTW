c0eaf1710e4631d72afedba1221fd0aeecde9c00
José Carlos <jose__carlos17@hotmail.com>
2016-11-27 14:16:04 +0000

    Restaurant info displays

--- src/templates/restaurant_info.php
+++ src/templates/restaurant_info.php
@@ -1,1 +1,23 @@
-<p>Price = <?=$info['price']?> </p>
+<?php if($info['price']) { ?>
+    <p>Price: <?=$info['price']?></p>
+<?php } else { ?>
+    <p>No prices available</p>
+<?php } ?>
+
+<?php if($info['category']) { ?>
+    <p>Categories: <?=$info['category']?></p>
+<?php } else { ?>
+    <p>No categories available</p>
+<?php } ?>
+
+<?php if($info['openHours']) { ?>
+    <p>Open Hours: <?=$info['openHours']?></p>
+<?php } else { ?>
+    <p>No open hours available</p>
+<?php } ?>
+
+<?php if($info['closeHours']) { ?>
+    <p>Close hours: <?=$info['closeHours']?></p>
+<?php } else { ?>
+    <p>No close hours available</p>
+<?php } ?>
