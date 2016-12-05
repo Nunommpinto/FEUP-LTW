c8d6d83467b4c7bfb99c92c95156756223bd3d9f
Fr0sk <the.fr0sk@gmail.com>
2016-12-05 17:55:14 +0000

    Added login and register forms to dropdown box

--- src/templates/header.php
+++ src/templates/header.php
@@ -8,7 +8,10 @@
                 integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
                 crossorigin="anonymous">
         </script>
+        <script type="text/javascript" src="../javascript/header.js"></script>
+        <script type="text/javascript" src="../javascript/login_dropdown.js"></script>
 		<link rel="stylesheet" href="../css/header.css">
+        <link rel="stylesheet" href="../css/login_dropdown.css">
     </head>
 
     <body>
@@ -16,15 +19,24 @@
             <h1>FEUPrant</h1>
             <h2>The definitive restaurant reviews website!</h2>
         </div>
-        <div id="nav-btns">
+        <div id="nav-btns" class="nav-btns">
             <ul>
-                <li><a href="../pages/homepage.php">Homepage</a></li>
-                <li><a href="../pages/top_restaurants.php">Top Restaurants</a></li>
+                <li><a href="homepage.php">Homepage</a></li>
+                <li><a href="#">Top Restaurants</a></li>
+                <li><a href="#">Write a Review</a></li>
                 <li><a href="myprofile.php">Profile</a></li>
-                <li><a href="../pages/search_restaurant.php">Search a restaurant</a></li>
-                <li><a href="../pages/add_restaurant.php">Add a new restaurant</a></li>
-                <div id="user-info"><a href="#"><?php echo(isset($username) ? $username : 'Login/Register') ?></a></div>  
+                <li><a href="search_restaurant.php">Search a restaurant</a></li>
+                <li><a href="add_restaurant.php">Add a new restaurant</a></li>
+                <div id="user-info">
+                    <?php
+                        if (isset($username)) {
+                            echo $username;
+                        } else { 
+                            include_once("../templates/login_dropdown.php");
+                        }
+                    ?>
+                </div>  
             </ul>
         </div>
-        <div id="user-info-content"><?php include_once('login_box.php'); ?></div>
-        <script type="text/javascript" src="templates/header.js"></script>
+
+        
