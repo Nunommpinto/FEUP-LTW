db784bb38198dc1564711986c8289297dcc3bf33
Nunommpinto <nunex_pinto@hotmail.com>
2016-12-07 13:30:13 +0000

    write review bug fixed

--- src/templates/write_review.php
+++ src/templates/write_review.php
@@ -1,9 +1,10 @@
 <div class="form-component">
     <form action="../database/action_write_review.php" method="post">
         <input type="hidden" name="idRestaurant" value="<?=$_GET['idRestaurant']?>">
-
+		<br>
         <label>Review: </label><textarea name="review" required="required" rows="6" cols="80" placeholder='Express your opinion about the restaurant...'></textarea>
-        <input type="number" name="rating">
+        Rating: <input type="number" name="rating">
+		<br>
         <input type="submit" value="Send">
     </form>
 </div>
