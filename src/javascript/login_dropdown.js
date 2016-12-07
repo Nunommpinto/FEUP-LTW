d0e824ca3e343d92befcdc7b69ec69f0194ba79f
Fr0sk <the.fr0sk@gmail.com>
2016-12-07 00:03:56 +0000

    Fixed javascript error when user logged in (definitely)

--- src/javascript/login_dropdown.js
+++ src/javascript/login_dropdown.js
@@ -1,22 +1,13 @@
 loginDropdownTablinks = function (tabName) {
-    var i, tabcontent, tablinks;
-    
     // Get all elements with class="login-dropdown-tabcontent" and hide them
-    tabcontent = document.getElementsByClassName("login-dropdown-tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
+    $('.login-dropdown-tabcontent').hide();
 
     // Get all elements with class="login-dropdown-tablinks" and remove the class "active"
-    tablinks = document.getElementsByClassName("login-dropdown-tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" header-elem-active", "");
-    }
+    $('login-dropdown-tablinks').removeClass('header-elem-active')
 
     // Show the current tab, and add an "active" class to the link that opened the tab
-    document.getElementById(tabName).style.display = "block";
-    document.getElementById(tabName + 'link').className += " header-elem-active";
-    //evt.currentTarget.className += " active";
+    $('#' + tabName).show();
+    $('#' + tabName + 'link').addClass('header-elem-active');
 }
 
 // Close the dropdown menu if the user clicks outside of it
@@ -32,18 +23,16 @@ $(document).mousedown(function (e)
 });
 
 $(document).ready(function() {
-    if (document.getElementsByTagName('login-dropdown').length > 0) {
-        // Add login/register link click handler
-        $('.login-dropdown').children('a').on('click', function() {
-            $('.login-dropdown').children('a').toggleClass('header-elem-active');
-            $('.login-dropdown-content').toggleClass('show');
-        });
+    // Add login/register link click handler
+    $('.login-dropdown').children('a').on('click', function() {
+        $('.login-dropdown').children('a').toggleClass('header-elem-active');
+        $('.login-dropdown-content').toggleClass('show');
+    });
 
-        // Add login and register tablinks functionality
-        $('#login-tablink').on('click', function() { loginDropdownTablinks('login-tab'); });
-        $('#register-tablink').on('click', function() { loginDropdownTablinks('register-tab'); });
+    // Add login and register tablinks functionality
+    $('#login-tablink').on('click', function() { loginDropdownTablinks('login-tab'); });
+    $('#register-tablink').on('click', function() { loginDropdownTablinks('register-tab'); });
 
-        // Open default tab
-        loginDropdownTablinks('login-tab'); 
-    }
+    // Open default tab
+    loginDropdownTablinks('login-tab'); 
 });
