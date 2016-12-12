ad9b67ed5962847d8acdf9f346600d0cd915778f
José Carlos <jose__carlos17@hotmail.com>
2016-12-12 21:40:21 +0000

    Dropdown with owner's restaurants option. Working on it.

--- src/templates/user_dropdown.php
+++ src/templates/user_dropdown.php
@@ -1,8 +1,17 @@
+<?php
+    include_once('../database/connection.php');
+    include_once('../database/db_user.php');
+    
+    $isOwner = getIsOwnerById(intval($_SESSION['idUser'][0]));
+    $isOwner = intval($isOwner[0]);?>
+
 <div class="user-dropdown">
     <a href="#"><?php echo $_SESSION['username']; ?></a>
     <div id="user-dropdown-content" class="user-dropdown-content">
         <a href="#">Profile</a>
-        <a href="#">Something</a>
+        <?php if($isOwner != 0) { ?>
+            <a href="#">My Restaurants</a>
+        <?php } ?>
         <a href="#">Something Else</a>
         <div class="separator"> </div>
         <a href="#">Settings</a>
