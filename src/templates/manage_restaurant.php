3b2e46ecb8a6295ff33b824ac02577b980fbc390
José Carlos <jose__carlos17@hotmail.com>
2016-12-12 23:16:08 +0000

    Management of restaurants is showing all the owner's restaurants and it's main information. Also, link are available.

--- src/templates/manage_restaurant.php
+++ src/templates/manage_restaurant.php
@@ -13,5 +13,18 @@
     $photo = getPhotoById($info['idRestaurantInfo']);*/
 
     $restaurants = getAllOwnedRestaurants(intval($_SESSION['idUser'][0]));
-    var_dump($restaurants);
+    foreach($restaurants as $restaurant) {
+        $info = getInfoById($restaurant['idRestaurantInfo']);
+        $localization = getLocalizationById($info['idLocalization']);
+        $photo = getPhotoById($info['idRestaurantInfo']);
 ?>
+
+<a href="../templates/restaurant.php?idRestaurant=<?=$restaurant['idRestaurant']?>">
+    <img src="../images/originals/<?=$photo['idPhoto']?>" alt="Restaurant" width="300" height="200"> <br>
+</a> <br>
+<a href="../templates/restaurant.php?idRestaurant=<?=$restaurant['idRestaurant']?>"><?=$restaurant['name']?></a> <br>
+<?=$localization['city']?> <br>
+<?=$localization['road']?> <br>
+<?=$localization['postalCode']?>
+<br> <br>
+<?php } ?>
