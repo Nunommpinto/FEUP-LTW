68758080fc72d1a09901215e815cc0863b47ff67
Nunommpinto <nunex_pinto@hotmail.com>
2016-12-13 16:26:35 +0000

    minor changes and css

--- src/pages/add_restaurant.php
+++ src/pages/add_restaurant.php
@@ -2,11 +2,14 @@
     include_once('../templates/header.php');
     include_once('../database/db_user.php');
 
+	if(!isset($_SESSION['username']))
+        die('<br /> You do not have access to this page. Please go back');
+	
     $isOwner = getIsOwnerById(intval($_SESSION['idUser'][0]));
     $isOwner = intval($isOwner[0]);
 
     if($isOwner != 0)
         include_once('../templates/add_restaurant.php');
     else
-        echo 'can\'t add';
+        echo '<br /> You do not have access to this page. Please go back.';
 ?>
