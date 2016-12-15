1c89cbc08d9d9547256d8d351f37e82d4ac2d598
José Carlos <jose__carlos17@hotmail.com>
2016-12-15 18:57:24 +0000

    Started ajaxing the search

--- src/pages/search_restaurant.php
+++ src/pages/search_restaurant.php
@@ -1,30 +1,34 @@
-<html>
-<head>
-<link rel="stylesheet" href="../css/search_restaurant.css">
-</head>
-</html>
-
 <?php
     include_once('../templates/header.php');
 ?>
 
-<form id="search" action="../database/action_search_restaurant.php" method="get">
+<script type="text/javascript" src="../javascript/search_restaurant.js"></script>
+
+<form class="form-component" action="javascript:void(0);">
     <h3> Insert the information above </h3>
-	<label>Name: </label> <input type="text" name="name">
+	<label>Name: </label>
+	<input id="search-input-name" class="search" type="text" name="name" value="Jose">
 	
 	<br><br>
-    <label>Min score: </label> <input type="number" name="min-score" min="0" max="5">
+    <label><b>Min score: </b></label> 
+	<input id="search-input-min" class="search" type="number" name="min-score" min="0" max="5">
+
 	<div id="max">
-	<label>Max score: </label> <input type="number" name="max-score" min="0" max="5">
+	<label>Max score: </label>
+	<input id="search-input-max" class="search" type="number" name="max-score" min="0" max="5">
 	</div>
 	
 	<br><br><br>
-    <label>Average Price: </label> <input type="number" name="price">
+    <label>Average Price: </label>
+	<input id="search-input-price" class="search" type="number" name="price">
 	<br><br>
 	
 	<div id="labels">
-    <label>Country: </label> <input type="text" name="country">
-    <label>City: </label> <input type="text" name="city">
+		<label>Country: </label>
+		<input id="search-input-country" class="search" type="text" name="country">
+
+		<label>City: </label>
+		<input id="search-input-city" class="search" type="text" name="city">
 	</div>
     <input type="submit" value="Search">
 </form>
